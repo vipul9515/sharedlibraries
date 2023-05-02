7d4e116c3bf42c7c8fcdbe4ce714fd077a740235
vipul9515 <130993451+vipul9515@users.noreply.github.com>
2023-05-02 11:22:32 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -14,3 +14,10 @@ def newDeploy()
 {
 deploy adapters: [tomcat9(credentialsId: '2e024ab6-cb3e-4e88-9651-a78810d0722d', path: '', url: 'http://172.31.32.228:8080')], contextPath: 'test1', war: '**/*.war'
 }
+
+def newTest()
+{
+git 'https://github.com/intelliqittrainings/FunctionalTesting.git'
+sh 'java -jar /var/lib/jenkins/workspace/DeclarativePipeline1/testing.jar'                    
+}
+
